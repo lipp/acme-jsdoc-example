1a063c9e516b7785eb01d7ed2df0e2e2b4172f63
Gerhard Preuss <gelipp@gmail.com>
2015-12-10 10:48:05 +0100

    add global info

--- src/acme.js
+++ src/acme.js
@@ -1,4 +1,29 @@
+/**
+ *
+ */
+
+/**
+ * @constant {string}
+ * @default
+ */
+var greet = 'who reads this is stupid';
+
+/**
+ * @constant {Object}
+ * @default
+ */
+var config = {
+    company: 'ACME TM'
+};
+
+/**
+ * Super useful function.
+ * @function
+ */
+var superFunc = function() {};
+
 module.exports = {
     funny: require('./funny'),
-    heavy: require('./heavy')
+    heavy: require('./heavy'),
+    greet: greet
 };
